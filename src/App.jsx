361efb80d828d51f075e507643c01d76145e0d88
RanjithS581 <ranjit.s.it.2021@snsce.ac.in>
2025-02-21 15:24:31 +0530

    added lorem file

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <>
       <h1 className='text-red-500 bg-blue-200'>Welcome to BookEase</h1>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae quisquam omnis labore ducimus sint suscipit temporibus quos quia nihil a! Veniam fugit optio possimus corrupti sit nihil facere recusandae. In.</h1>
     </>
   )
 }
